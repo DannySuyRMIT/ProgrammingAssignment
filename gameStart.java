6b62ed48b5ea1bd8e6245ab35668361b5dda6423
S4091714-Yomu <S4091714@students.rmit.edu.au>
2025-05-26 00:25:38 +1000

    Final gameStart.java

--- gameStart.java
+++ gameStart.java
@@ -1,27 +1,30 @@
 import util.userScanner;
 
 public class gameStart {
-    public static void main(String [] args) {
-        System.out.print("- - - Game Name - - -\n");
-        System.out.print("Game Setting...");
-        System.out.print("Select your choice:\n1. Begin\n2. Retreat\n");
+    public static void main(String[] args) {
+        System.out.print(" - - - - - Youkai Daze - - - - -\n"); // Title Sequence!
+
+        // Lore block
+        System.out.print("\nAfter a night of heavy drinking with your fellow workers, just to cope with\n the news of being laid off from work. You head out, walking down the hazy\n streets of Tokyo, your surroundings becoming a blur from the effects of alcohol. \nLured down an alleyway, you spot an old, small shrine, just as you pass out...\n\n");
+        System.out.print("Waking up to an old wooden pier, your eyes adjust to the dark and humid atmosphere \naround you. The waters around you move about suspiciously, its' depths hidden underneath the surface. \nThe air around you feels... different. Mist surrounds you, and with how obscured your vision is, \nthe only choice you have is heading up the pier...\n");
+
+        // Choice to begin or exit.
+        System.out.print("Select your choice:\n1. Begin\n2. Exit\n");
         userScanner userInput = new userScanner();
         String userChoice = userInput.userScan();
-        while (userChoice != "begin" || userChoice != "retreat") {
+        while (true) {
             if (userChoice.equals("begin") || userChoice.equals("1")) {
                 System.out.print("Beginning Simulation...\n");
                 // Put gameCore / gameLogic method here
+                gameCore.gameLogic();
                 break;
-            }
-            else if(userChoice.equals("retreat") || userChoice.equals("2")) {
+            } else if (userChoice.equals("exit") || userChoice.equals("2")) {
                 System.out.print("\n The Matrix.");
-                System.exit(1);
-            }
-            else {
+                System.exit(0);
+            } else {
                 System.out.print("Please Select an option:");
                 userChoice = userInput.userScan();
             }
         }
-
     }
 }
