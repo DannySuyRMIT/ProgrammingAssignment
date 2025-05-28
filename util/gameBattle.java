71f1dacf5afb136d87b9442d11b94cef549ba043
DannySuyRMIT <S4171942@student.rmit.edu.au>
2025-05-28 21:00:23 +1000

    Game Battle turn base

--- util/gameBattle.java
+++ util/gameBattle.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class BattleTest {
+public class gameBattle {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -10,18 +10,25 @@ public class BattleTest {
 
         // Create a player using the entered name
         gamePlayer player = new gamePlayer(playerName, 50, 3);
-        gameEnemy enemy = new gameEnemy("Goblin", 15, 2);
+        gameEnemy enemy = new gameEnemy("dsada", 15, 2);
 
         // Battle loop: Player and enemy take turns attacking
         while (player.isAlive() && enemy.isAlive()) {
             // Player's turn
             System.out.println("\nIt's your turn!");
             System.out.println("1. Attack");
+            System.out.println("2. Inventory");
             System.out.print("Choose an action: ");
             int action = scanner.nextInt();
 
             if (action == 1) {
                 player.attack(enemy);
+            } else if (action == 2) {
+                player.showInventory();
+                System.out.print("Choose an item to use (1-8): ");
+            } else {
+                System.out.println("Invalid action. Please choose 1 or 2.");
+                continue;
             }
 
             // Check if the enemy is dead
